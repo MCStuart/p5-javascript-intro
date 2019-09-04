b3c89da5272e7c8fd4ca75950825a56f6bbef29c
Stuart Mckay <stuart51994@gmail.com>
2019-09-03 22:32:50 -0700

    displa cat.jpg with async

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,39 +1,8 @@
-let x = 0;
-
 function setup() {
-  // put setup code here
-  drawingContext.shadowOffsetX = 5;
-  drawingContext.shadowOffsetY = -5;
-  drawingContext.shadowBlur = 10;
-  drawingContext.shadowColor = "black";
-  background(200);
-  ellipse(width/2, height/2, 50, 50);
+  createCanvas(400, 240);
+  loadImage('cat.jpg', drawCat);
 }
 
-function draw() {
-  // put drawing code here
-  // ellipse(x, height/2, 20, 20);
-  // x++;
+function drawCat(img) {
+  image(img, 0, 0);
 }
-
-// mouse values and function calls
-// MOUSE VALUES
-// mouseX
-// mouseY
-// MOUSE ACTION
-// mouseIsPressed 	
-// MOUSE FUNCTIONS
-// mousePressed()
-// mouseMoved() 	
-// mouseDragged()
-// mouseReleased()
-// mouseClicked() 	
-// mouseScrolled() 	
-
-// TOUCH VALUES
-// touchX
-// touchY
-// touches[]
-// TOUCH FUNCTIONS
-// touchStarted()
-// touchMoved()
