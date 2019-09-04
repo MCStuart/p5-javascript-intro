a02f31afe12adaa9a8b8da4788cae87fa300eb72
Stuart McKay <stuart51994@gmail.com>
2019-09-03 20:24:42 -0700

    make circle with dropshadow

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -2,12 +2,16 @@ let x = 0;
 
 function setup() {
   // put setup code here
-  let myCanvas = createCanvas(600, 400);
-  myCanvas.parent('myContainer');
+  drawingContext.shadowOffsetX = 5;
+  drawingContext.shadowOffsetY = -5;
+  drawingContext.shadowBlur = 10;
+  drawingContext.shadowColor = "black";
+  background(200);
+  ellipse(width/2, height/2, 50, 50);
 }
 
 function draw() {
   // put drawing code here
-  ellipse(x, height/2, 20, 20);
-  x++;
+  // ellipse(x, height/2, 20, 20);
+  // x++;
 }
